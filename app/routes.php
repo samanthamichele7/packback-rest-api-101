5fab79791d4ed21ef62ab3b5ee1a1ad649b87a58
Samantha Geitz <samanthamichele7@gmail.com>
2014-11-17 21:54:16 -0600

    Step 4 - add routes, version 2 with explicit code

--- app/routes.php
+++ app/routes.php
@@ -17,18 +17,29 @@ Route::get('/', function()
 });
 
 /**
- * Route::resource automatically looks for create, edit, index, show, store, update, and destroy
- * methods in your controllers without having to define them here
- *
- * It's less code, but it can be confusing if you have a lot of resources and custom routes
- *
- * See http://laravel.com/docs/4.2/controllers#restful-resource-controllers for more info
+ * Explicit routes for each of the controller methods you need in your API
  *
  * Grouped within an API prefix so that you have to access it through http://url.com/api/...
  *
  */
 
 Route::group(['prefix' => 'api'], function() {
-    Route::resource('users', 'UserController');
-    Route::resource('books', 'BookController');
+    Route::group(['prefix' => 'books'], function(){
+        Route::get('', 				    array('uses' => 'BookController@index'));
+        Route::get('{id}', 			    array('uses' => 'BookController@show'));
+        Route::post('', 			    array('before' => 'auth', 'uses' => 'BookController@store'));
+        Route::patch('{id}', 			array('before' => 'auth', 'uses' => 'BookController@update'));
+        Route::delete('{id}',		    array('before' => 'auth', 'uses' => 'BookController@destroy'));
+    });
+
+    Route::group(['prefix' => 'users'], function(){
+        Route::get('', 				        array('uses' => 'UserController@index'));
+        Route::get('{id}', 			        array('uses' => 'UserController@show'));
+        Route::get('{id}/books',            array('uses' => 'UserController@showBooks'));
+        Route::post('', 			        array('before' => 'auth', 'uses' => 'UserController@store'));
+        Route::post('{id}/books',           array('uses' => 'UserController@storeBooks'));
+        Route::patch('{id}', 			    array('before' => 'auth', 'uses' => 'UserController@update'));
+        Route::delete('{id}',		        array('before' => 'auth', 'uses' => 'UserController@destroy'));
+        Route::delete('{id}/books/{id}',    array('uses' => 'UserController@destroyBooks'));
+    });
 });
