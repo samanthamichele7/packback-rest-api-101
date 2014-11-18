81a6f51d0097ecfd1da144a8026348b0ca3e0d63
Samantha Geitz <samanthamichele7@gmail.com>
2014-11-17 21:44:42 -0600

    Step 4 - add routes, version 1 with less code

--- app/routes.php
+++ app/routes.php
@@ -15,3 +15,20 @@ Route::get('/', function()
 {
 	return View::make('hello');
 });
+
+/**
+ * Route::resource automatically looks for create, edit, index, show, store, update, and destroy
+ * methods in your controllers without having to define them here
+ *
+ * It's less code, but it can be confusing if you have a lot of resources and custom routes
+ *
+ * See http://laravel.com/docs/4.2/controllers#restful-resource-controllers for more info
+ *
+ * Grouped within an API prefix so that you have to access it through http://url.com/api/...
+ *
+ */
+
+Route::group(['prefix' => 'api'], function() {
+    Route::resource('users', 'UserController');
+    Route::resource('books', 'BookController');
+});
